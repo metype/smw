e129c4cd86aa98de5bd616b07f288f851dcdaedf
MotionControls <bprogers43@tntech.edu>
2023-09-21 11:11:05 -0500

    Added vars related to Escape Challenge in CustomPhys.h.

--- src/CustomPhys.h
+++ src/CustomPhys.h
@@ -16,6 +16,13 @@ int* cc_coinTimer_Max;      // Time in-between each drain in seconds.
 int* cc_coinTimer_Cur;      // Time before next coin is drained.
 int* cc_disableLevelTimer;  // Whether or not to disable the level timer.
 
+/* Escape Challenge Vars */
+int* ec_finishedLevel;              // Whether or not player has finished the level.
+uint8* ec_finishHortRange;          // How close Player has to be to the left-most side of the level to complete it after escape.
+int* ec_escapeTimer;                // Time until un-timely demise.
+uint8* pizzaFace_x, *pizzaFace_y;   // God do I even want to attempt this?
+uint8* pizzaFace_w, *pizzaFace_h;   // Size of pizza face.
+
 /* Phys Funcs */
 void PHYS_DoCelDash(int8,int8,int);
 void PHYS_DoDoubleJump(int8,int);
